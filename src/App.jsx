7349bad0da97464cac9349b8b74f3eb2b98dd6cd
Lucas Kolinger <lucaskolinger@icloud.com>
2023-02-09 23:49:33 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Moon from "./Moon";
+import darkImage from "./assets/dark.svg";
+import lightImage from "./assets/light.svg";
+import { useState } from "react";
+
+function App() {
+  const [isDarkmode, switchIsDarkmode] = useState(true);
+
+  return (
+    <div>
+      <Moon />
+      {isDarkmode ? (
+        <img className="bg-image" src={darkImage} alt="" />
+      ) : (
+        <img className="bg-image" src={lightImage} alt="" />
+      )}
+    </div>
+  );
+}
+
+export default App;
